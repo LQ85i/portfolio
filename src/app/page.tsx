72a50d74f4f9dbee94e18ed31104e18f00603653
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-09-08 07:20:47 +0300

    Remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ import Intro from "./sections/Intro";
 import Footer from "./sections/Footer";
 
 export default function Page() {
-  //redirect to new url
-  if (window.location.href === "https://lq85i.github.io/portfolio/") {
-    window.location.href = "https://portfolio-lq85i.vercel.app/";
-  }
   return (
     <main className="flex h-max overflow-x-hidden overflow-y-visible flex-col items-center xs:pt-24 pt-10">
       <Intro />
