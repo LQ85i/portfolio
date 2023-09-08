fc46bc43278c50610b7b123159226f7abb72cbe6
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-09-08 06:45:27 +0300

    Edit config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // required for gh-pages deployment
   images: { unoptimized: true },
   compiler: {
     // Enables the styled-components SWC transform
